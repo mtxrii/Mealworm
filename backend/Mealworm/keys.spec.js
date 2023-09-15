45d73842cb6f4c86fc7b39d8a78987f025bb480a
Enrique <edavalos@ucsc.edu>
2023-09-15 15:26:26 -0700

    Added template keys file

---
+++ backend/Mealworm/keys.spec.js
@@ -0,0 +1,9 @@
+const yelpApiKey = 'apiKey';
+const googleApiKey = 'apiKey';
+const positionStackApiKey = 'apiKey';
+const mealwormApiKey = 'apiKey';
+
+exports.yelpApiKey = yelpApiKey;
+exports.googleApiKey = googleApiKey;
+exports.positionStackApiKey = positionStackApiKey;
+exports.mealwormApiKey = mealwormApiKey;
