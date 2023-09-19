36e2bd5c53b5c06edd5e5927ddcf0084fdad83bd
Enrique <edavalos@ucsc.edu>
2023-09-18 17:12:50 -0700

    Added random catchphrase generation

--- frontend/Mealworm/src/components/LandingLogo.js
+++ frontend/Mealworm/src/components/LandingLogo.js
@@ -1,13 +1,23 @@
 import logo from '../assets/noun-worm-3088557.svg';
 import './LandingLogo.css';
 
+const APP_CATCHPHRASES = [
+  "Find somewhere new to eat",
+  "Find random cool restaurants near you",
+  "Get some new food suggestions"
+]
+
+function getRandomCatchphrase() {
+  return APP_CATCHPHRASES[Math.floor((Math.random()*APP_CATCHPHRASES.length))];
+}
+
 function App() {
   return (
     <div className="App-logo-container">
       <img src={logo} className="App-logo-img" alt="logo" />
       <div className="App-logo-text">
         <h1 className="App-logo-title">Mealworm</h1>
-        <h3 className="App-logo-subtitle">Find somewhere new to eat</h3>
+        <h3 className="App-logo-subtitle">{getRandomCatchphrase()}</h3>
       </div>
     </div>
   );
