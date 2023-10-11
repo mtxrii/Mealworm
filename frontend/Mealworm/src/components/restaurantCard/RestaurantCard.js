706fdf316c5e8c8de44776bc114d440cda907dac
Enrique <edavalos@ucsc.edu>
2023-10-10 23:31:39 -0700

    Added card template to RestaurantCard.js

--- frontend/Mealworm/src/components/restaurantCard/RestaurantCard.js
+++ frontend/Mealworm/src/components/restaurantCard/RestaurantCard.js
@@ -1,7 +1,28 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
 export default function RestaurantCard(props) {
     return (
-        <div>
-            {props.idx + ". " + props.name}
-        </div>
+        // <div>
+        //     {props.idx + ". " + props.name}
+        // </div>
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+            sx={{ height: 140 }}
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+        </Card>
     );
 }
