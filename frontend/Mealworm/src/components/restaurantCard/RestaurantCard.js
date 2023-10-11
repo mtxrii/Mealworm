963c417fb4ae32ce49095f8f4d8e78571a05ed4b
Enrique <edavalos@ucsc.edu>
2023-10-11 00:03:58 -0700

    Added template image
    for when no image is provided

--- frontend/Mealworm/src/components/restaurantCard/RestaurantCard.js
+++ frontend/Mealworm/src/components/restaurantCard/RestaurantCard.js
@@ -2,14 +2,15 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import defaultRestaurantImage from '../../assets/zakaria-zayane-0uAVsDcyD0M-unsplash.jpg'
 
 export default function RestaurantCard(props) {
     return (
         <Card sx={{ maxWidth: 345 }} style={{ margin: '12px' }}>
             <CardMedia
             sx={{ height: 140 }}
-            image="/static/images/cards/contemplative-reptile.jpg"
-            title="green iguana"
+            image={defaultRestaurantImage}
+            title={props.name}
             />
             <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
