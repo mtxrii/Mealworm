fe71bffe60f83f8c8c82b11807eb39afcaf13d7f
Enrique <edavalos@ucsc.edu>
2023-10-11 01:40:02 -0700

    Added price and rating in restaurant card descriptions

--- frontend/Mealworm/src/components/restaurantCard/RestaurantCard.js
+++ frontend/Mealworm/src/components/restaurantCard/RestaurantCard.js
@@ -1,8 +1,14 @@
+// Components
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import defaultRestaurantImage from '../../assets/zakaria-zayane-0uAVsDcyD0M-unsplash.jpg'
+import Chip from '@mui/material/Chip';
+import StarIcon from '@mui/icons-material/Star';
+import { Grid } from '@mui/material';
+// Other files
+import defaultRestaurantImage from '../../assets/zakaria-zayane-0uAVsDcyD0M-unsplash.jpg';
+import CONFIG from '../../app-config.json';
 
 export default function RestaurantCard(props) {
     const noImg = props.img === null;
@@ -18,11 +24,25 @@ export default function RestaurantCard(props) {
                 <Typography gutterBottom variant="h5" component="div">
                     {props.idx + ". " + props.name}
                 </Typography>
-                <Typography variant="body2" color="text.secondary" align="left">
-                    123 Sample St
-                    <br/>
-                    San Jose, CA 95113
-                </Typography>
+
+                <Grid
+                container
+                direction="row"
+                style={{ marginTop: "10px" }}
+                >
+                    <Grid item xs={6} align="left">
+                        <Chip label="$ $ $ $" variant="outlined" />
+                        <div style={{ margin: "5px" }}/>
+                        <Chip label={<StarIcon />} variant="outlined" />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="body1" color={CONFIG.buttonColor} align="left">
+                            123 Sample St
+                            <br/>
+                            San Jose, CA 95113
+                        </Typography>
+                    </Grid>
+                </Grid>
             </CardContent>
         </Card>
     );
