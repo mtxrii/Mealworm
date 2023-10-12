cdad226728b99ce9ac207563022bede7c26a1bf5
Enrique <edavalos@ucsc.edu>
2023-10-11 22:14:00 -0700

    Made app bar black

--- frontend/Mealworm/src/components/filtersSelectionBar/FiltersSelectionBar.js
+++ frontend/Mealworm/src/components/filtersSelectionBar/FiltersSelectionBar.js
@@ -2,8 +2,17 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { DEFAULT_KEYWORD } from '../../fetchRequest/dataGather';
 import CONFIG from '../../app-config.json';
+
+const theme = createTheme({
+  palette: {
+      primary: {
+          main: CONFIG.footerColor
+      }
+  }
+});
 
 export default function FiltersSelectionBar(props) {
   const location = props.location == DEFAULT_KEYWORD ? CONFIG.searchFilterDefaults.locationDefault : props.location;
@@ -11,13 +20,15 @@ export default function FiltersSelectionBar(props) {
   const cuisine  = props.cuisine  == DEFAULT_KEYWORD ? CONFIG.searchFilterDefaults.cuisineDefault  : props.cuisine;
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            pinIcon: '{location}' distanceIcon: '{distance} mi' searchIcon: '{props.isUsingGoogle ? "Google" : "Yelp"}' cuisineIcon: '{cuisine}'
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              pinIcon: '{location}' distanceIcon: '{distance} mi' searchIcon: '{props.isUsingGoogle ? "Google" : "Yelp"}' cuisineIcon: '{cuisine}'
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
     </Box>
   );
 }
